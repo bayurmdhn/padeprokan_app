d90af1df45d678938480a20b62db54f271e32bc1
Ahmad Sabiq Taqiyuddin <sabiqtaqiyuddin2@gmail.com>
2022-07-13 10:17:23 +0700

    succes

--- lib/Screens/HomePage/homePage.dart
+++ lib/Screens/HomePage/homePage.dart
@@ -151,23 +151,26 @@ class HomePage extends StatelessWidget {
 }
 
 Widget _buildPopupDialog(BuildContext context) {
-  return new AlertDialog(
-    title: const Text('Popup example'),
-    content: new Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text("Hello"),
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: new AlertDialog(
+      title: const Text('Popup example'),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Hello"),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Close'),
+        ),
       ],
     ),
-    actions: <Widget>[
-      new FlatButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        textColor: Theme.of(context).primaryColor,
-        child: const Text('Close'),
-      ),
-    ],
   );
 }
