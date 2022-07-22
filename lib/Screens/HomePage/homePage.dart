4de918b4fe2205ee8f75385a3c1b61d211423cd1
TaraTakki <sabiqtaqiyuddin2@gmail.com>
2022-07-22 15:10:23 +0700

    succes

--- lib/Screens/HomePage/homePage.dart
+++ lib/Screens/HomePage/homePage.dart
@@ -3,6 +3,8 @@ import 'package:path/path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_profile_picture/flutter_profile_picture.dart';
 
+import '../../Components/appBar.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -18,151 +20,7 @@ class _HomePageState extends State<HomePage> {
         home: Scaffold(
           //AppBar Padeprokan
           appBar: AppBar(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new Image.asset('assets/images/Logo padeprokan.png'),
-                  new Text(
-                    "Spaces",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 18,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.bold),
-                  ),
-
-                  //Profile Picture button and Notification button
-                  Row(
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.only(right: 17),
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.notifications,
-                              color: Color.fromARGB(255, 138, 138, 138),
-                            ),
-                            onPressed: () {
-                              showDialog(
-                                  context: context,
-                                  builder: (context) => Padding(
-                                        padding: const EdgeInsets.only(
-                                            right: 54, top: 30),
-                                        child: AlertDialog(
-                                          alignment: Alignment.topRight,
-                                          content: Container(
-                                            width: 227,
-                                            height: 256,
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              children: [
-                                                Image.asset(
-                                                    'assets/images/Vector.png'),
-                                                Text(
-                                                  "No Data",
-                                                  style: TextStyle(
-                                                      fontSize: 18,
-                                                      color: Color.fromARGB(
-                                                          255, 138, 138, 138)),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ),
-                                      ));
-                            },
-                          )),
-
-                      //Account Button
-                      IconButton(
-                          onPressed: () {
-                            showDialog(
-                                context: context,
-                                builder: (context) => Padding(
-                                      padding: const EdgeInsets.fromLTRB(
-                                          12, 23, 24, 34),
-                                      child: AlertDialog(
-                                        alignment: Alignment.topRight,
-                                        content: Container(
-                                          height: 100,
-                                          width: 200,
-                                          child: Column(
-                                            children: [
-                                              Row(
-                                                children: [
-                                                  Icon(
-                                                    Icons.circle_rounded,
-                                                    color: Color.fromARGB(
-                                                        255, 138, 138, 138),
-                                                  ),
-                                                  Padding(
-                                                    padding:
-                                                        const EdgeInsets.only(
-                                                            left: 13),
-                                                    child: Text(
-                                                      "My Account",
-                                                      style: TextStyle(
-                                                          fontSize: 14),
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                              Divider(
-                                                color: Color.fromARGB(
-                                                    255, 138, 138, 138),
-                                                thickness: 1,
-                                              ),
-                                              Row(
-                                                children: [
-                                                  Text("Change Password",
-                                                      style: TextStyle(
-                                                          fontSize: 14)),
-                                                ],
-                                              ),
-                                              Divider(
-                                                color: Color.fromARGB(
-                                                    255, 138, 138, 138),
-                                                thickness: 1,
-                                              ),
-                                              Row(
-                                                children: [
-                                                  Text("Logout",
-                                                      style: TextStyle(
-                                                          fontSize: 14))
-                                                ],
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ));
-                          },
-                          icon: Icon(
-                            Icons.circle_rounded,
-                            color: Color.fromARGB(255, 138, 138, 138),
-                          ))
-                    ],
-                  ),
-                ],
-              ),
-              // actions: [
-              //   Row(
-              //     children: [
-              //       PopupMenuButton(
-              //           icon: Icon(
-              //             Icons.circle_rounded,
-              //             color: Color.fromARGB(255, 138, 138, 138),
-              //           ),
-              //           itemBuilder: (context) => [
-              //                 PopupMenuItem(
-              //                   child: Text("First"),
-              //                   value: 1,
-              //                 )
-              //               ]),
-              //     ],
-              //   ),
-              // ],
+              title: appBarClass(),
               backgroundColor: Color.fromARGB(255, 255, 255, 255),
               centerTitle: true),
           //MainBody
