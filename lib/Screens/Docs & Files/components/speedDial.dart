edde09eed97d198959039c5cb7e13dd781a1d42b
arjunazaki <arjunazaki93@gmail.com>
2022-07-28 14:05:10 +0700

    Sukses

--- lib/Screens/Docs & Files/components/speedDial.dart
+++ lib/Screens/Docs & Files/components/speedDial.dart
@@ -70,182 +70,189 @@ class speedDial extends StatelessWidget {
                 showDialog(
                     context: context,
                     builder: (context) {
-                      final height = MediaQuery.of(context).size.height;
-                      return Material(
-                        color: Color.fromARGB(0, 0, 0, 0),
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                              top: 183, right: 16, left: 16, bottom: 350),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: kWhite,
-                                borderRadius: BorderRadius.circular(8)),
-                            child: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Padding(
-                                      padding:
-                                          EdgeInsets.only(left: 17, top: 21),
-                                      child: Text(
-                                        "Embed Link From google drive",
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontFamily: 'Roboto',
-                                            color: kblack),
-                                      ),
+                      return
+                          // return Material(
+                          //   color: Color.fromARGB(0, 0, 0, 0),
+                          //   // child: Padding(
+                          //   //   padding: const EdgeInsets.only(
+                          //   //       top: 183, right: 16, left: 16),
+                          //   child:
+                          Padding(
+                        padding: const EdgeInsets.only(
+                            top: 183, left: 16, right: 16, bottom: 300),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: kWhite,
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 17, top: 21),
+                                    child: Text(
+                                      "Embed Link From google drive",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontFamily: 'Roboto',
+                                          color: kblack),
                                     ),
-                                    Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 21, right: 21),
+                                  ),
+                                  Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 21, right: 21),
+                                      child: Material(
+                                        color: Color.fromARGB(0, 0, 0, 0),
                                         child: IconButton(
                                           onPressed: () {},
                                           icon: Icon(Icons.close),
-                                        ))
-                                  ],
-                                ),
-                                // Row(
-                                //   children: [
-                                //     Padding(
-                                //       padding: EdgeInsets.only(left: 17, top: 21),
-                                //       child: Text(
-                                //         "Embed Link From google drive",
-                                //         style: TextStyle(
-                                //             fontSize: 16,
-                                //             fontFamily: 'Roboto',
-                                //             color: kblack),
-                                //       ),
-                                //     ),
-                                //   ],
-                                // ),
-                                Divider(
-                                  height: 16,
-                                  color: kdivider,
-                                  thickness: 1,
-                                ),
-                                SizedBox(
-                                  height: 6,
-                                ),
-                                Row(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 17),
-                                      child: Text(
-                                        "File name",
-                                        style: embedLink,
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 3.5),
-                                      child: Text(
-                                        "*",
-                                        style: embedLink2,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 15,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 18, right: 22),
-                                  child: Material(
-                                    child: Form(
-                                      child: SizedBox(
-                                        height: 29,
-                                        child: TextFormField(
-                                          decoration: InputDecoration(
-                                            enabledBorder: OutlineInputBorder(
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(5)),
-                                                borderSide: BorderSide(
-                                                    color: Color.fromRGBO(
-                                                        140, 79, 225, 1))),
-                                            focusedBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    width: 2,
-                                                    color: Color.fromRGBO(
-                                                        140, 79, 225, 1))),
-                                            hintText: "Untitled",
-                                            hintStyle: searchFiles,
-                                            filled: true,
-                                            fillColor: Colors.white,
-                                          ),
-                                          style: TextStyle(fontSize: 12),
                                         ),
+                                      )),
+                                ],
+                              ),
+                              // Row(
+                              //   children: [
+                              //     Padding(
+                              //       padding: EdgeInsets.only(left: 17, top: 21),
+                              //       child: Text(
+                              //         "Embed Link From google drive",
+                              //         style: TextStyle(
+                              //             fontSize: 16,
+                              //             fontFamily: 'Roboto',
+                              //             color: kblack),
+                              //       ),
+                              //     ),
+                              //   ],
+                              // ),
+                              Divider(
+                                height: 16,
+                                color: kdivider,
+                                thickness: 1,
+                              ),
+                              SizedBox(
+                                height: 6,
+                              ),
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 17),
+                                    child: Text(
+                                      "File name",
+                                      style: embedLink,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 3.5),
+                                    child: Text(
+                                      "*",
+                                      style: embedLink2,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 18, right: 22),
+                                child: Material(
+                                  child: Form(
+                                    child: SizedBox(
+                                      height: 29,
+                                      child: TextFormField(
+                                        decoration: InputDecoration(
+                                          enabledBorder: OutlineInputBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(5)),
+                                              borderSide: BorderSide(
+                                                  color: Color.fromRGBO(
+                                                      140, 79, 225, 1))),
+                                          focusedBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  width: 2,
+                                                  color: Color.fromRGBO(
+                                                      140, 79, 225, 1))),
+                                          hintText: "Untitled",
+                                          hintStyle: searchFiles,
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                        ),
+                                        style: TextStyle(fontSize: 12),
                                       ),
                                     ),
                                   ),
                                 ),
-                                SizedBox(
-                                  height: 12,
-                                ),
-                                Row(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 17),
-                                      child: Text(
-                                        "Embed link",
-                                        style: embedLink,
-                                      ),
+                              ),
+                              SizedBox(
+                                height: 12,
+                              ),
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 17),
+                                    child: Text(
+                                      "Embed link",
+                                      style: embedLink,
                                     ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 3.5),
-                                      child: Text(
-                                        "*",
-                                        style: embedLink2,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 15,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 18, right: 22),
-                                  child: Material(
-                                    child: Form(
-                                      child: SizedBox(
-                                        height: 29,
-                                        child: TextFormField(
-                                          decoration: InputDecoration(
-                                            enabledBorder: OutlineInputBorder(
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(5)),
-                                                borderSide: BorderSide(
-                                                    color: Color.fromRGBO(
-                                                        140, 79, 225, 1))),
-                                            focusedBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    width: 2,
-                                                    color: Color.fromRGBO(
-                                                        140, 79, 225, 1))),
-                                            hintText:
-                                                "e.g.https://drive.google.com/open?id=1cMc-qT_W......",
-                                            hintStyle: searchFiles,
-                                            filled: true,
-                                            fillColor: Colors.white,
-                                          ),
-                                          style: TextStyle(fontSize: 12),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 3.5),
+                                    child: Text(
+                                      "*",
+                                      style: embedLink2,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 18, right: 22),
+                                child: Material(
+                                  child: Form(
+                                    child: SizedBox(
+                                      height: 29,
+                                      child: TextFormField(
+                                        decoration: InputDecoration(
+                                          enabledBorder: OutlineInputBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(5)),
+                                              borderSide: BorderSide(
+                                                  color: Color.fromRGBO(
+                                                      140, 79, 225, 1))),
+                                          focusedBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  width: 2,
+                                                  color: Color.fromRGBO(
+                                                      140, 79, 225, 1))),
+                                          hintText:
+                                              "e.g.https://drive.google.com/open?id=1cMc-qT_W......",
+                                          hintStyle: searchFiles,
+                                          filled: true,
+                                          fillColor: Colors.white,
                                         ),
+                                        style: TextStyle(fontSize: 12),
                                       ),
                                     ),
                                   ),
                                 ),
-                                Divider(
-                                  height: 25,
-                                  color: kdivider,
-                                  thickness: 1,
-                                )
-                              ],
-                            ),
+                              ),
+                              Divider(
+                                height: 25,
+                                color: kdivider,
+                                thickness: 1,
+                              )
+                            ],
                           ),
                         ),
                       );
+                      // ),
+                      // );
                     });
               }),
         ]);
