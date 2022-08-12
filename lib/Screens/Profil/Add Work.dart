51b1fe630574c8afd76b63240c29081e089ad2bb
havidsetya <havidsetya@gmail.com>
2022-08-12 13:45:19 +0700

    p

--- lib/Screens/Profil/Add Work.dart
+++ lib/Screens/Profil/Add Work.dart
@@ -155,6 +155,9 @@ class _AddWorkState extends State<AddWork> {
                                                     255, 140, 79, 225)))),
                                     style: TextStyle(fontSize: 11),
                                   )),
+                              Column(
+                                children: [MyStatefulWidget()],
+                              )
                             ],
                           ),
                         ),
@@ -183,6 +186,69 @@ class _AddWorkState extends State<AddWork> {
         decoration: BoxDecoration(
             color: Colors.orange, borderRadius: BorderRadius.circular(8)),
       ),
+    );
+  }
+}
+
+class LabeledCheckbox extends StatelessWidget {
+  const LabeledCheckbox({
+    Key? key,
+    required this.label,
+    required this.padding,
+    required this.value,
+    required this.onChanged,
+  }) : super(key: key);
+
+  final String label;
+  final EdgeInsets padding;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onChanged(!value);
+      },
+      child: Padding(
+        padding: padding,
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Text(label)),
+            Checkbox(
+              value: value,
+              onChanged: (bool? newValue) {
+                onChanged(newValue!);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  bool _isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return LabeledCheckbox(
+      label: 'I Still Work Here',
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      value: _isSelected,
+      onChanged: (bool newValue) {
+        setState(() {
+          _isSelected = newValue;
+        });
+      },
     );
   }
 }
