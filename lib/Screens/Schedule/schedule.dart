ee2f5876a5b0c1f16e677e335363cc8e521759b8
TaraTakki <sabiqtaqiyuddin2@gmail.com>
2022-08-16 16:14:52 +0700

    success

--- lib/Screens/Schedule/schedule.dart
+++ lib/Screens/Schedule/schedule.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
+import 'dart:ui';
 
 import 'package:first_app_flutter/Components/appBarBack.dart';
+import 'package:first_app_flutter/Screens/Schedule/addEvent.dart';
 
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:flutter/material.dart';
@@ -47,99 +49,171 @@ class _SchedulePageState extends State<SchedulePage> {
                           Padding(
                             padding: const EdgeInsets.only(top: 20),
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                GestureDetector(
-                                  onTap: () {
-                                    tampilanKalender = CalendarView.day;
-                                    setState(() {});
-                                  },
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        border: Border.all(
-                                          color: Color.fromARGB(
-                                              255, 122, 122, 122),
-                                        ),
-                                        borderRadius: BorderRadius.circular(3)),
-                                    width: 73,
-                                    height: 34,
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          'Day',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        )
-                                      ],
-                                    ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Day',
+                                    style: TextStyle(color: Colors.black),
                                   ),
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              Colors.white)),
                                 ),
-                                GestureDetector(
-                                  onTap: () {
-                                    tampilanKalender = CalendarView.month;
-                                    setState(() {});
-                                  },
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        border: Border.all(
-                                          color: Color.fromARGB(
-                                              255, 122, 122, 122),
-                                        ),
-                                        borderRadius: BorderRadius.circular(3)),
-                                    width: 73,
-                                    height: 34,
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          'Month',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        )
-                                      ],
-                                    ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Month',
+                                    style: TextStyle(color: Colors.black),
                                   ),
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              Colors.white)),
                                 ),
-                                GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      tampilanKalender = CalendarView.week;
-                                    });
-                                  },
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        border: Border.all(
-                                          color: Color.fromARGB(
-                                              255, 122, 122, 122),
-                                        ),
-                                        borderRadius: BorderRadius.circular(3)),
-                                    width: 73,
-                                    height: 34,
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          'Week',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        )
-                                      ],
-                                    ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Week',
+                                    style: TextStyle(color: Colors.black),
                                   ),
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              Colors.white)),
                                 ),
+                                // GestureDetector(
+                                //   onTap: () {
+                                //     setState(() {
+                                //       tampilanKalender = CalendarView.day;
+                                //     });
+                                //   },
+                                //   child: Container(
+                                //     decoration: BoxDecoration(
+                                //         color: Colors.white,
+                                //         border: Border.all(
+                                //           color: Color.fromARGB(
+                                //               255, 122, 122, 122),
+                                //         ),
+                                //         borderRadius: BorderRadius.circular(3)),
+                                //     width: 73,
+                                //     height: 34,
+                                //     child: Column(
+                                //       mainAxisAlignment:
+                                //           MainAxisAlignment.center,
+                                //       children: [
+                                //         Text(
+                                //           'Day',
+                                //           style: TextStyle(
+                                //               fontWeight: FontWeight.bold),
+                                //         )
+                                //       ],
+                                //     ),
+                                //   ),
+                                // ),
+                                // GestureDetector(
+                                //   onTap: () {
+                                //     setState(() {
+                                //       tampilanKalender = CalendarView.month;
+                                //     });
+                                //   },
+                                //   child: Container(
+                                //     decoration: BoxDecoration(
+                                //         color: Colors.white,
+                                //         border: Border.all(
+                                //           color: Color.fromARGB(
+                                //               255, 122, 122, 122),
+                                //         ),
+                                //         borderRadius: BorderRadius.circular(3)),
+                                //     width: 73,
+                                //     height: 34,
+                                //     child: Column(
+                                //       mainAxisAlignment:
+                                //           MainAxisAlignment.center,
+                                //       children: [
+                                //         Text(
+                                //           'Month',
+                                //           style: TextStyle(
+                                //               fontWeight: FontWeight.bold),
+                                //         )
+                                //       ],
+                                //     ),
+                                //   ),
+                                // ),
+                                // GestureDetector(
+                                //   onTap: () {
+                                //     setState(() {
+                                //       tampilanKalender = CalendarView.week;
+                                //     });
+                                //   },
+                                //   child: Container(
+                                //     decoration: BoxDecoration(
+                                //         color: Colors.white,
+                                //         border: Border.all(
+                                //           color: Color.fromARGB(
+                                //               255, 122, 122, 122),
+                                //         ),
+                                //         borderRadius: BorderRadius.circular(3)),
+                                //     width: 73,
+                                //     height: 34,
+                                //     child: Column(
+                                //       mainAxisAlignment:
+                                //           MainAxisAlignment.center,
+                                //       children: [
+                                //         Text(
+                                //           'Week',
+                                //           style: TextStyle(
+                                //               fontWeight: FontWeight.bold),
+                                //         )
+                                //       ],
+                                //     ),
+                                //   ),
+                                // ),
                               ],
                             ),
                           ),
-                          SfCalendar(
-                            view: tampilanKalender,
-                            initialSelectedDate: DateTime.now(),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(color: Colors.black)),
+                              child: SfCalendar(
+                                view: tampilanKalender,
+                                initialSelectedDate: DateTime.now(),
+                                monthViewSettings: MonthViewSettings(
+                                    monthCellStyle: MonthCellStyle(
+                                        backgroundColor:
+                                            Color.fromARGB(255, 194, 194, 194),
+                                        trailingDatesBackgroundColor:
+                                            Color.fromARGB(255, 112, 112, 112),
+                                        leadingDatesBackgroundColor:
+                                            Color.fromARGB(255, 112, 112, 112),
+                                        todayBackgroundColor:
+                                            Color.fromARGB(255, 178, 209, 255),
+                                        textStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 12,
+                                            fontFamily: 'Arial'),
+                                        todayTextStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                            fontFamily: 'Arial'),
+                                        trailingDatesTextStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontStyle: FontStyle.italic,
+                                            fontSize: 12,
+                                            fontFamily: 'Arial'),
+                                        leadingDatesTextStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontStyle: FontStyle.italic,
+                                            fontSize: 12,
+                                            fontFamily: 'Arial'))),
+                              ),
+                            ),
                           )
                         ],
                       ),
@@ -150,12 +224,15 @@ class _SchedulePageState extends State<SchedulePage> {
             )
           ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showDialog(context: context, builder: (context) => AlertDialog());
-          },
-          child: Icon(Icons.add_rounded),
-          backgroundColor: Color.fromARGB(255, 255, 162, 2),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 78),
+          child: FloatingActionButton(
+            onPressed: () {
+              showDialog(context: context, builder: (context) => addEvent());
+            },
+            child: Icon(Icons.add_rounded),
+            backgroundColor: Color.fromARGB(255, 255, 162, 2),
+          ),
         ),
       ),
     );
