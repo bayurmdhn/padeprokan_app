e19b2749c07cc01bd76ffcb5955801f2fc6c5774
TaraTakki <sabiqtaqiyuddin2@gmail.com>
2022-10-04 12:48:02 +0700

    success

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return SplashScreenView(
-      navigateRoute: videoConference(),
+      navigateRoute: HomePage(),
       duration: 3000,
       imageSize: 100,
       imageSrc: "assets/images/logo.png",
