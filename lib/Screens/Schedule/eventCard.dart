7b31da6fb0454499443e1c87fb7e446400ed49ed
TaraTakki <sabiqtaqiyuddin2@gmail.com>
2022-10-24 10:26:54 +0700

    success

--- lib/Screens/Schedule/eventCard.dart
+++ lib/Screens/Schedule/eventCard.dart
@@ -2,10 +2,6 @@ import '../../Screens/Schedule/schedule.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-String editEvent = '';
-
-enum MenuItem { item1, item2 }
-
 class eventCard extends StatefulWidget {
   const eventCard({Key? key}) : super(key: key);
 
@@ -16,82 +12,97 @@ class eventCard extends StatefulWidget {
 class _eventCardState extends State<eventCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 260,
-      height: 42,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-              .withOpacity(1.0)),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 5),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  content: Container(
+                      width: 260,
+                      height: 350,
+                      child: Column(
                         children: [
-                          Text(
-                            '$eventName',
-                            style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          PopupMenuButton(
-                            itemBuilder: (context) => [
-                              PopupMenuItem(
-                                  value: MenuItem.item1,
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.edit,
-                                        color: Color.fromARGB(255, 0, 102, 255),
-                                      ),
-                                      Text(
-                                        'Edit',
-                                        style: TextStyle(
-                                            color: Color.fromARGB(
-                                                255, 0, 102, 255)),
-                                      )
-                                    ],
-                                  )),
-                              PopupMenuItem(
-                                  value: MenuItem.item2,
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.delete,
-                                        color: Color.fromARGB(255, 246, 0, 0),
-                                      ),
-                                      Text('Delete',
-                                          style: TextStyle(
-                                              color: Color.fromARGB(
-                                                  255, 246, 0, 0)))
-                                    ],
-                                  ))
+                          Row(
+                            children: [
+                              Text(
+                                '$eventName',
+                                style: TextStyle(fontSize: 12),
+                              ),
                             ],
                           ),
+                          Divider(
+                            thickness: 1,
+                            color: Color.fromARGB(255, 159, 159, 159),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Deskripsi:',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Color.fromARGB(255, 53, 53, 53),
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 15),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 0),
+                                  child: Text('$eventDescription',
+                                      style: TextStyle(fontSize: 12)),
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
-                      ),
-                    ))
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child:
-                      Text('$eventDescription', style: TextStyle(fontSize: 8)),
-                ),
-              ],
-            )
-          ],
+                      )),
+                ));
+      },
+      child: Container(
+        width: 260,
+        height: 42,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+                .withOpacity(1.0)),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: ListView(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              '$eventName',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ))
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(DateTime.now().toString()),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
